240fda1cb427225541f86d1d3c502c574ae92e2f
XcenaX <vlad-057@mail.ru>
2021-03-06 18:29:52 +0600

    static

--- almazmed/settings.py
+++ almazmed/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/c/cn26973/Almazmed/static/"
+STATIC_ROOT = "/home/c/cn26973/Almazmed/main/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
